0ae334218be0b5449987920b936c01d50edc6540
Luna Crafts <lunacrafts@protonmail.com>
2023-02-16 11:04:11 +0100

    Mirrorboards Auth - bootstrap + auth route

--- auth/auth/src/index.ts
+++ auth/auth/src/index.ts
@@ -1,1 +1,21 @@
-console.log('test');
+import dotenv from 'dotenv';
+import express from 'express';
+import logger from 'morgan';
+import debug from 'debug';
+
+// Routes
+import auth from './auth/auth.routes';
+
+// Service
+dotenv.config();
+
+const log = debug('auth');
+const app = express();
+
+app.use(logger('dev'));
+
+app.use('/auth', auth);
+
+app.listen(4000, () => {
+  log('Listening on 4000');
+});
