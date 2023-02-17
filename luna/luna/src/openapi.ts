87ad62c21610223a627ea1d562cc87b1a75000af
Luna Crafts <lunacrafts@protonmail.com>
2023-02-17 11:23:35 +0100

    Luna API: Auth

--- luna/luna/src/openapi.ts
+++ luna/luna/src/openapi.ts
@@ -2,9 +2,9 @@ import { generateOpenApiDocument } from 'trpc-openapi';
 import { router } from './router';
 
 export const openApiDocument = generateOpenApiDocument(router, {
-  title: 'Mirrorboards Auth',
+  title: 'Luna',
   description: 'Mirrorboards Authentication Services',
   version: '1.0.0',
   baseUrl: 'http://localhost:4000/api',
-  tags: ['auth'],
+  tags: ['auth', 'authenticators'],
 });
--- luna/luna/src/router/index.ts
+++
@@ -1,6 +0,0 @@
-import { t } from "../trpc";
-import { auth } from "./auth";
-
-export const router = t.router({ auth });
-
-export type AppRouter = typeof router;
