69c54d1703efbc654f1c023c1a9dbeb4edb53bab
Luna Crafts <lunacrafts@protonmail.com>
2023-02-21 14:46:19 +0100

    Narnia Auth: Luna -> Narnia -> setCookie auth / 1

--- mirrorboards/mirrorboards/src/routes/shell/shell.component.tsx
+++ mirrorboards/mirrorboards/src/routes/shell/shell.component.tsx
@@ -2,19 +2,17 @@ import { Outlet } from "@tanstack/react-router"
 import React from "react"
 import { narnia } from "../../narnia";
 
+// narnia.auth.login.mutate({
+//   email: 'lunacrafts@protonmail.com',
+//   password: 'crafts'
+// });
+
+narnia.mirrorboards.mirrorboards.findAll.query().then((res) => {
+  console.log('res');
+  console.log(res);
+})
+
 export const ShellComponent = () => {
-  React.useEffect(() => {
-    narnia.auth.login.mutate({
-      email: 'lunacrafts@protonmail.com',
-      password: 'crafts'
-    });
-
-    narnia.mirrorboards.mirrorboards.findAll.query().then((res) => {
-      console.log('res');
-      console.log(res);
-    })
-  }, []);
-
   return <div>
     Mirrorboards Shell
     <div style={{ border: '1px solid black' }}>
