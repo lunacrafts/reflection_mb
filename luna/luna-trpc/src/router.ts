951d7dee3dca9d56d701b683721e4d0afdcf0311
Luna Crafts <lunacrafts@protonmail.com>
2023-02-28 17:32:45 +0100

    Luna tRPC: Move all procedures to luna-trpc

--- luna/luna-trpc/src/router.ts
+++ luna/luna-trpc/src/router.ts
@@ -1,7 +1,8 @@
 import { t } from "./trpc";
 
 import { router as authenticators } from './routers/authenticators/authenticators.router';
+import { router as mirrorboards } from './routers/mirrorboards/mirrorboards.router';
 
-export const router = t.router({ authenticators });
+export const router = t.router({ authenticators, mirrorboards });
 
 export type LunaRouter = typeof router;
