aa9df73bb15857756bc8434645da048cba321fac
Luna Crafts <lunacrafts@protonmail.com>
2023-03-06 14:43:30 +0100

    Mirrorboards: Add Tailwindcss

--- mirrorboards/mirrorboards/src/routes/boards/boards.component.tsx
+++ mirrorboards/mirrorboards/src/routes/boards/boards.component.tsx
@@ -1,4 +1,3 @@
-import { Outlet } from "@tanstack/react-router"
 import { luna } from "../../trpc/luna.trpc"
 
 export const BoardsComponent = () => {
@@ -8,55 +7,13 @@ export const BoardsComponent = () => {
     getNextPageParam: (lastPage) => lastPage.nextCursor,
   });
 
-  const create = luna.mirrorboards.create.useMutation({
-    onSuccess: () => {
-      mirrorboards.refetch();
-    }
-  });
-
-  const makePublic = luna.mirrorboards.promote.useMutation({
-    onSuccess: () => {
-      mirrorboards.refetch();
-    }
-  });
-
-  if (mirrorboards.isLoading) {
-    return <div>spinner</div>
-  }
-
-  return <div>
-    <div>Boards:</div>
-    <div>
-      {mirrorboards.data?.pages.map((page) => {
+  return <div className="bg-black bg-gradient-to-r from-black via-slate-900/50 to-black h-full">
+    {
+      mirrorboards.data?.pages.map((page) => {
         return page.mirrorboards.map((item) => {
           return <div key={item.id}>{item.title}</div>
         })
-      })}
-    </div>
-    {/* {JSON.stringify(mirrorboards.data)} */}
-    <Outlet />
-
-    <div onClick={() => {
-      mirrorboards.fetchNextPage();
-    }}>Fetch next page</div>
-
-    <div>Create public:</div>
-    <button onClick={() => create.mutate({
-      isPublic: true,
-      title: 'Second mirrorboard!'
-    })}>create</button>
-
-    <hr />
-
-    <div>
-      <button onClick={() => {
-        makePublic.mutate({
-          id: '01GTERZ7VYAV9QBT6JGA890JS6',
-          isPromoted: true,
-        });
-      }}>
-        Promote
-      </button>
-    </div>
+      })
+    }
   </div>
 }
