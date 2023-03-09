db661bd6f353a8d2b06e5f2efbf3e9e6dd84e9ae
Luna Crafts <lunacrafts@protonmail.com>
2023-03-09 12:06:47 +0100

    Mirrorboards: Boards grid

--- mirrorboards/mirrorboards/src/routes/boards/boards.component.tsx
+++ mirrorboards/mirrorboards/src/routes/boards/boards.component.tsx
@@ -1,3 +1,6 @@
+import { motion } from 'framer-motion';
+import { Link } from '@tanstack/react-router';
+import { FontAwesomeIcon } from "@reflection/icons";
 import { luna } from "../../trpc/luna.trpc"
 
 export const BoardsComponent = () => {
@@ -7,13 +10,32 @@ export const BoardsComponent = () => {
     getNextPageParam: (lastPage) => lastPage.nextCursor,
   });
 
-  return <div className="bg-black bg-gradient-to-r from-black via-slate-900/50 to-black h-full">
-    {
-      mirrorboards.data?.pages.map((page) => {
-        return page.mirrorboards.map((item) => {
-          return <div key={item.id}>{item.title}</div>
+  return <div className="h-full w-full bg-black bg-gradient-to-r from-black via-slate-900/50 to-black">
+    <div className="grid gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+      {
+        mirrorboards.data?.pages.map((page) => {
+          return page.mirrorboards.map((mirrorboard, index) => {
+            return <motion.div
+              key={mirrorboard.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: index * 0.003 }}>
+              <Link
+                to={`/shell/mirrorboard/$mirrorboardId`}
+                params={{
+                  mirrorboardId: mirrorboard.id,
+                }}>
+                <div className="drop-shadow-sm bg-white/10 hover:bg-white/20 focus:bg-white/20 transition-all text-white rounded-md flex flex-row items-center">
+                  <FontAwesomeIcon className="opacity-25 p-4" icon="paw" color={"white"} fontSize={30} />
+                  <div className="text-sm subpixel-antialiased" style={{ color: "hsl(215deg 10% 83%)" }}>
+                    {mirrorboard.title}
+                  </div>
+                </div>
+              </Link>
+            </motion.div>
+          })
         })
-      })
-    }
+      }
+    </div>
   </div>
 }
