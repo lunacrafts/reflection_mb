a99a5dd57d28021ea0cf51e817fd88742f6adb55
Luna Crafts <lunacrafts@protonmail.com>
2023-03-15 14:29:01 +0100

    Mirrorboards: Commands

--- mirrorboards/mirrorboards/src/extensions/mirrorboards/extension/MirrorboardsExtension.tsx
+++ mirrorboards/mirrorboards/src/extensions/mirrorboards/extension/MirrorboardsExtension.tsx
@@ -1,9 +1,7 @@
-import { MirrorboardsExtensionCommands } from "./MirrorboardsExtension.commands";
 import { MirrorboardsExtensionReflection } from "./MirrorboardsExtension.reflection";
 
 export const MirrorboardsExtension = () => {
   return <>
     <MirrorboardsExtensionReflection />
-    <MirrorboardsExtensionCommands />
   </>
 }
