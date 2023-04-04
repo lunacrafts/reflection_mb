892d0f615e94f909de345b7f25baef4e5bf1e8c9
Luna Crafts <lunacrafts@protonmail.com>
2023-04-04 18:14:52 +0200

    ReflectionLayout / Fix content overflow

--- mirrorboards/mirrorboards/src/extensions/hypeboards/routes/index/index.component.tsx
+++ mirrorboards/mirrorboards/src/extensions/hypeboards/routes/index/index.component.tsx
@@ -2,7 +2,6 @@ import { BoardLayout } from "@reflection-layouts/board"
 
 export const HypeboardsIndexComponent = () => {
   return <div style={{ height: '100%', backgroundColor: '#181818' }}>
-    HypeboardsIndexComponent
     <BoardLayout />
   </div>
 }
